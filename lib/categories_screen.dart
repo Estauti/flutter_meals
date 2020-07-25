a53cbbdc5fd7eaa1318e8c00ed14b5f5631821ba
Estauti <estautib@gmail.com>
2020-07-25 18:03:23 -0300

    Wrap categories screen with scaffold and use as main screen

--- lib/categories_screen.dart
+++ lib/categories_screen.dart
@@ -7,18 +7,23 @@ import './models/dummy_data.dart';
 class CategoriesScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return GridView(
-      children: DUMMY_CATEGORIES
-          .map((c) => CategoryItem(
-                title: c.title,
-                color: c.color,
-              ))
-          .toList(),
-      gridDelegate: SliverGridDelegateWithMaxCrossAxisExtent(
-          maxCrossAxisExtent: 200,
-          childAspectRatio: 3 / 2,
-          crossAxisSpacing: 20,
-          mainAxisSpacing: 20),
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('DeliMeals'),
+      ),
+      body: GridView(
+        children: DUMMY_CATEGORIES
+            .map((c) => CategoryItem(
+                  title: c.title,
+                  color: c.color,
+                ))
+            .toList(),
+        gridDelegate: SliverGridDelegateWithMaxCrossAxisExtent(
+            maxCrossAxisExtent: 200,
+            childAspectRatio: 3 / 2,
+            crossAxisSpacing: 20,
+            mainAxisSpacing: 20),
+      ),
     );
   }
 }
